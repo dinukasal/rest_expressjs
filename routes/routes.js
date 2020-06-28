abef1b6b2b6087df072089166b51e9d5508f39dc
Dinuka Salwathura <dinukasal@gmail.com>
2020-06-28 10:13:54 +0530

    added get / and logs

--- routes/routes.js
+++ routes/routes.js
@@ -1,19 +1,26 @@
-var appRouter = function(app) {
+var appRouter = function (app) {
 
-app.get("/", function(req, res) {
-    res.send("Hello World");
-}); 
+    app.get("/", function (req, res) {
+        console.log(req.query);
+        console.log(req.body);
+        res.send({ status: "ok" });
+    });
 
-app.post("/account", function(req, res) {
-    if(!req.body.username || !req.body.password || !req.body.twitter) {
-        return res.send({"status": "error", "message": "missing a parameter"});
-    } else {
-        return res.send(req.body);
-    }
-});
+    app.post("/", function (req, res) {
+        console.log(req.body);
+        res.send("post");
+    });
+
+    app.post("/account", function (req, res) {
+        if (!req.body.username || !req.body.password || !req.body.twitter) {
+            return res.send({ "status": "error", "message": "missing a parameter" });
+        } else {
+            return res.send(req.body);
+        }
+    });
 
 }
- 
+
 module.exports = appRouter;
 
 
